f78e0da9649fde1dcbbb49e1f0f413a6dc3557dc
Shibiyarasu K B <162137995+Shibiyarasu-KB@users.noreply.github.com>
2025-03-07 15:24:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (73.71%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,4 +1,11 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return bin(int(a,2)+int(b,2))[2:]
+        #where the bin indicates the binary
+
+        #the int (a , 2) indicates that the given value and it's conversion base number as 2
+
+        #this will convert the binary to inyteger
+
+        # now the both integer added , and the bin converts the integer into binary value , and there will be '0b' which indicates it as a binary , so we print it after 2 index
         
